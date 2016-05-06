55eec586666dc23e5a990806b47c0df701668a5f
oneliang <oneliang@tencent.com>
2016-05-06 23:15:41 +0800

    update RSA.java

--- src/main/java/com/oneliang/util/encrypt/RSA.java
+++ src/main/java/com/oneliang/util/encrypt/RSA.java
@@ -1,12 +1,18 @@
 package com.oneliang.util.encrypt;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 import javax.crypto.Cipher;
 
@@ -17,8 +23,10 @@ import com.oneliang.util.file.FileUtil;
 
 public final class RSA {
 
-	private static final String ALGORITHM_RSA = "RSA";
-	private static final String RSA_MODE_1 = "RSA/ECB/PKCS1Padding";
+	public static final String ALGORITHM_RSA = "RSA";
+	public static final String RSA_MODE_1 = "RSA/ECB/PKCS1Padding";
+	public static final int ENCRYPT_MAX_LENGTH=117;
+	public static final int DECRYPT_MAX_LENGTH=128;
 
 	private RSA() {}
 
@@ -37,26 +45,56 @@ public final class RSA {
 
 	/**
 	 * @param byteArray
-	 * @param privateKey
+	 * @param key
 	 * @return byte[]
 	 * @throws Exception
 	 */
-	public static byte[] encryptByPrivateKey(byte[] byteArray, PrivateKey privateKey) throws Exception {
+	public static byte[] encryptByKey(byte[] byteArray, Key key) throws Exception {
+		ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
 		Cipher cipher = Cipher.getInstance(ALGORITHM_RSA);
-		cipher.init(Cipher.ENCRYPT_MODE, privateKey);
-		return cipher.doFinal(byteArray);
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		if(byteArray!=null){
+			int leftLength=byteArray.length;
+			ByteArrayInputStream byteArrayInputStream=new ByteArrayInputStream(byteArray);
+			while(leftLength>0){
+				int bufferLength=ENCRYPT_MAX_LENGTH;
+				if(leftLength<ENCRYPT_MAX_LENGTH){
+					bufferLength=leftLength;
+				}
+				byte[] buffer=new byte[bufferLength];
+				int length=byteArrayInputStream.read(buffer, 0, buffer.length);
+				leftLength=leftLength-length;
+				byteArrayOutputStream.write(cipher.doFinal(buffer));
+			}
+		}
+		return byteArrayOutputStream.toByteArray();
 	}
 
 	/**
 	 * @param byteArray
-	 * @param publicKey
+	 * @param key
 	 * @return byte[]
 	 * @throws Exception
 	 */
-	public static byte[] decryptByPublicKey(byte[] byteArray, PublicKey publicKey) throws Exception {
+	public static byte[] decryptByKey(byte[] byteArray, Key key) throws Exception {
+		ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
 		Cipher cipher = Cipher.getInstance(RSA_MODE_1);
-		cipher.init(Cipher.DECRYPT_MODE, publicKey);
-		return cipher.doFinal(byteArray);
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		if(byteArray!=null){
+			int leftLength=byteArray.length;
+			ByteArrayInputStream byteArrayInputStream=new ByteArrayInputStream(byteArray);
+			while(leftLength>0){
+				int bufferLength=DECRYPT_MAX_LENGTH;
+				if(leftLength<DECRYPT_MAX_LENGTH){
+					bufferLength=leftLength;
+				}
+				byte[] buffer=new byte[bufferLength];
+				int length=byteArrayInputStream.read(buffer, 0, buffer.length);
+				leftLength=leftLength-length;
+				byteArrayOutputStream.write(cipher.doFinal(buffer));
+			}
+		}
+		return byteArrayOutputStream.toByteArray();
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -75,19 +113,21 @@ public final class RSA {
 		publicOutputStream.write(publicKeyBase64ByteArray);
 		privateOutputStream.close();
 		publicOutputStream.close();
-//		byte[] buffer = Base64.decode(privateKeyBase64);
-//		PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(buffer);
-//		KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM_RSA);
-//		privateKey = keyFactory.generatePrivate(pkcs8KeySpec);
-//
-//		buffer = Base64.decode(publicKeyBase64);
-//		keyFactory = KeyFactory.getInstance(ALGORITHM_RSA);
-//		X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(buffer);
-//		publicKey = keyFactory.generatePublic(x509KeySpec);
+		byte[] buffer = Base64.decode(privateKeyBase64ByteArray, Base64.DEFAULT);
+		PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(buffer);
+		KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM_RSA);
+		privateKey = keyFactory.generatePrivate(pkcs8KeySpec);
 
-		String string = "a";
-		byte[] encryptByteArray = encryptByPrivateKey(string.getBytes(Constant.Encoding.UTF8), privateKey);
+		buffer = Base64.decode(publicKeyBase64ByteArray, Base64.DEFAULT);
+		keyFactory = KeyFactory.getInstance(ALGORITHM_RSA);
+		X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(buffer);
+		publicKey = keyFactory.generatePublic(x509KeySpec);
+		//need to encrypt data max length:117
+		//need to decrypt data max length:128
+		String string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+		System.out.println(string.getBytes(Constant.Encoding.UTF8).length);
+		byte[] encryptByteArray = encryptByKey(string.getBytes(Constant.Encoding.UTF8), privateKey);
 		System.out.println(StringUtil.byteArrayToHexString(encryptByteArray));
-		System.out.println(new String(decryptByPublicKey(encryptByteArray, publicKey), Constant.Encoding.UTF8));
+		System.out.println(new String(decryptByKey(encryptByteArray, publicKey), Constant.Encoding.UTF8));
 	}
 }
