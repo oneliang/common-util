0427b8b47b623a2e89f08f4ec23711023591da70
oneliang <oneliang@tencent.com>
2016-05-14 21:21:20 +0800

    add pattern match to LoggerManager.java

--- src/main/java/com/oneliang/util/logging/LoggerManager.java
+++ src/main/java/com/oneliang/util/logging/LoggerManager.java
@@ -1,7 +1,11 @@
 package com.oneliang.util.logging;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+import com.oneliang.util.common.StringUtil;
 
 /**
  * @author oneliang
@@ -9,6 +13,8 @@ import java.util.concurrent.ConcurrentHashMap;
 public final class LoggerManager {
 
 	private static final Map<Class<?>, Logger> loggerMap=new ConcurrentHashMap<Class<?>, Logger>();
+	private static final Map<String, Logger> loggerPatternMap=new ConcurrentHashMap<String, Logger>();
+	private static final Set<String> loggerPatternSet=new ConcurrentSkipListSet<String>();
 
 	/**
 	 * get logger
@@ -16,7 +22,19 @@ public final class LoggerManager {
 	 * @return Logger
 	 */
 	public static Logger getLogger(Class<?> clazz) {
+		if(clazz==null){
+			throw new NullPointerException("parameter[Class<?> class] can no be null");
+		}
 		Logger logger = loggerMap.get(clazz);
+		if(logger==null){
+			String className=clazz.getName();
+			for(String patternKey:loggerPatternSet){
+				if(StringUtil.isMatchPattern(className, patternKey)){
+					logger=loggerPatternMap.get(patternKey);
+					break;
+				}
+			}
+		}
 		return logger == null ? new BaseLogger(Logger.Level.ERROR) : logger;
 	}
 
@@ -30,10 +48,29 @@ public final class LoggerManager {
 	}
 
 	/**
+	 * register logger
+	 * @param pattern
+	 * @param logger
+	 */
+	public static void registerLogger(String pattern, Logger logger){
+		loggerPatternMap.put(pattern, logger);
+		loggerPatternSet.add(pattern);
+	}
+
+	/**
 	 * unregister logger
 	 * @param clazz
 	 */
 	public static void unregisterLogger(Class<?> clazz){
 		loggerMap.remove(clazz);
+	}
+
+	/**
+	 * unregister logger
+	 * @param pattern
+	 */
+	public static void unregisterLogger(String pattern){
+		loggerPatternMap.remove(pattern);
+		loggerPatternSet.remove(pattern);
 	}
 }
