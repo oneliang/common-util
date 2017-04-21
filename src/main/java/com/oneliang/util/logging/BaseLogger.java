e3f3ae07c58e9be78235ba9f55a9a23d5ac551e1
oneliang <oneliang@tencent.com>
2017-04-21 15:06:26 +0800

    add thread name to log

--- src/main/java/com/oneliang/util/logging/BaseLogger.java
+++ src/main/java/com/oneliang/util/logging/BaseLogger.java
@@ -6,47 +6,54 @@ import com.oneliang.util.common.TimeUtil;
 
 public class BaseLogger extends AbstractLogger {
 
-	/**
-	 * constructor
-	 * @param level
-	 */
-	public BaseLogger(Level level) {
-		super(level);
-	}
+    /**
+     * constructor
+     * 
+     * @param level
+     */
+    public BaseLogger(Level level) {
+        super(level);
+    }
 
-	/**
-	 * log
-	 * @param level
-	 * @param message
-	 * @param throwable
-	 */
-	protected void log(Level level, Object message, Throwable throwable){
-		System.out.println(processMessage(level, message, throwable));
-		if(throwable!=null){
-			throwable.printStackTrace();
-		}
-	}
+    /**
+     * log
+     * 
+     * @param level
+     * @param message
+     * @param throwable
+     */
+    protected void log(Level level, Object message, Throwable throwable) {
+        System.out.println(processMessage(level, message, throwable));
+        if (throwable != null) {
+            throwable.printStackTrace();
+        }
+    }
 
-	/**
-	 * process message
-	 * @param level
-	 * @param message
-	 * @param throwable
-	 * @return String
-	 */
-	protected String processMessage(Level level, Object message, Throwable throwable){
-		StringBuilder stringBuilder=new StringBuilder();
-		stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_LEFT);
-		stringBuilder.append(TimeUtil.dateToString(TimeUtil.getTime(), TimeUtil.YEAR_MONTH_DAY_HOUR_MINUTE_SECOND_MILLISECOND));
-		stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_RIGHT);
-		stringBuilder.append(StringUtil.SPACE);
-		stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_LEFT);
-		stringBuilder.append(level.name());
-		stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_RIGHT);
-		stringBuilder.append(StringUtil.SPACE);
-		stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_LEFT);
-		stringBuilder.append(message);
-		stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_RIGHT);
-		return stringBuilder.toString();
-	}
+    /**
+     * process message
+     * 
+     * @param level
+     * @param message
+     * @param throwable
+     * @return String
+     */
+    protected String processMessage(Level level, Object message, Throwable throwable) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_LEFT);
+        stringBuilder.append(TimeUtil.dateToString(TimeUtil.getTime(), TimeUtil.YEAR_MONTH_DAY_HOUR_MINUTE_SECOND_MILLISECOND));
+        stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_RIGHT);
+        stringBuilder.append(StringUtil.SPACE);
+        stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_LEFT);
+        stringBuilder.append(level.name());
+        stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_RIGHT);
+        stringBuilder.append(StringUtil.SPACE);
+        stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_LEFT);
+        stringBuilder.append(Thread.currentThread().getName());
+        stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_RIGHT);
+        stringBuilder.append(StringUtil.SPACE);
+        stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_LEFT);
+        stringBuilder.append(message);
+        stringBuilder.append(Constant.Symbol.MIDDLE_BRACKET_RIGHT);
+        return stringBuilder.toString();
+    }
 }
