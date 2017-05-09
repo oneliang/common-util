ee506493c16b27b79317fe111e9289361e3b505c
oneliang <oneliang@tencent.com>
2017-05-09 10:25:41 +0800

    file logger support append

--- src/main/java/com/oneliang/util/logging/FileLogger.java
+++ src/main/java/com/oneliang/util/logging/FileLogger.java
@@ -12,44 +12,44 @@ import com.oneliang.util.file.FileUtil;
 
 public class FileLogger extends BaseLogger {
 
-	private FileOutputStream fileOutputStream=null;
+    private FileOutputStream fileOutputStream = null;
 
-	public FileLogger(Level level, File outputFile) {
-		super(level);
-		if(outputFile==null){
-			throw new NullPointerException("outputFile can not be null.");
-		}else{
-			try {
-				FileUtil.createFile(outputFile.getAbsolutePath());
-				this.fileOutputStream=new FileOutputStream(outputFile);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public FileLogger(Level level, File outputFile) {
+        super(level);
+        if (outputFile == null) {
+            throw new NullPointerException("outputFile can not be null.");
+        } else {
+            try {
+                FileUtil.createFile(outputFile.getAbsolutePath());
+                this.fileOutputStream = new FileOutputStream(outputFile, true);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	protected void log(Level level, Object message, Throwable throwable) {
-		String messageString=this.processMessage(level, message, throwable)+StringUtil.CRLF_STRING;
-		try {
-			this.fileOutputStream.write(messageString.getBytes(Constant.Encoding.UTF8));
-			if(throwable!=null){
-				throwable.printStackTrace(new PrintStream(this.fileOutputStream));
-			}
-			this.fileOutputStream.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    protected void log(Level level, Object message, Throwable throwable) {
+        String messageString = this.processMessage(level, message, throwable) + StringUtil.CRLF_STRING;
+        try {
+            this.fileOutputStream.write(messageString.getBytes(Constant.Encoding.UTF8));
+            if (throwable != null) {
+                throwable.printStackTrace(new PrintStream(this.fileOutputStream));
+            }
+            this.fileOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	protected void finalize() throws Throwable {
-		super.finalize();
-		try{
-			if(this.fileOutputStream!=null){
-				this.fileOutputStream.flush();
-				this.fileOutputStream.close();
-			}
-		}catch(IOException e){
-			e.printStackTrace();
-		}
-	}
+    protected void finalize() throws Throwable {
+        super.finalize();
+        try {
+            if (this.fileOutputStream != null) {
+                this.fileOutputStream.flush();
+                this.fileOutputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
